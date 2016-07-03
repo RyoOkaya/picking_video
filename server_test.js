cecb2bfd90071c5c524688ed6f90edf049f99433
noifuji <ryoma1025@gmail.com>
2016-07-03 18:10:15 +0000

    fix server.js

--- server_test.js
+++ server_test.js
@@ -3,6 +3,6 @@ var http = require("http");
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
-}).listen(8124);
+}).listen(process.env.Port, process.env.IP);
 
-console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at ' + process.env.IP + ':' + process.env.Port);
