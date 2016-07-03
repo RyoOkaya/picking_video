c0040b2cbd122f10ca1834667037131eefcb6f57
noifuji <ryoma1025@gmail.com>
2016-07-03 18:23:00 +0000

    make it run node.js

--- server_test.js
+++ server_test.js
@@ -1,8 +1,7 @@
-var http = require("http");
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(process.env.PORT, process.env.IP);
 
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(process.env.Port, process.env.IP);
-
-console.log('Server running at ' + process.env.IP + ':' + process.env.Port);
+console.log('Server running at ' + process.env.IP + ':' + process.env.PORT);
