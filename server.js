acbc7de5e3cec445c5461f408836cf54d0ecb2f5
noifuji <ryoma1025@gmail.com>
2016-07-13 15:27:08 +0000

    change server ip

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ router.get('/positions', function(req, res) {
         });
 });
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+server.listen(process.env.PORT || 8080, function() {
     var addr = server.address();
     console.log("server listening at", addr.address + ":" + addr.port);
 });
